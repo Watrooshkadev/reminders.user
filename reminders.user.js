abc5052b3cb992b9cb6ac198f9f4c82ee14bf81b
Watrooshkadev <101659049+Watrooshkadev@users.noreply.github.com>
2026-01-22 09:17:10 +0300

    Update reminders.user.js

--- reminders.user.js
+++ reminders.user.js
@@ -2,7 +2,7 @@
 // ==UserScript==
 // @name         Reminders (Local Config, SPA)
 // @namespace    reminders_local
-// @version      3.6
+// @version      3.7
 // @description  Напоминания для сайтов + большое центральное окно
 // @author       Watrooshka
 // @updateURL    https://raw.githubusercontent.com/Watrooshkadev/reminders.user/refs/heads/main/reminders.user.js
